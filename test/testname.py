59bfd2554e5c62de7d207f32e673b006220ffc6d
Keir <Keirhearn@gmail.com>
2021-06-09 12:35:18 +0100

    added test

---
+++ test/testname.py
@@ -0,0 +1,14 @@
+
+from example_python_package_Blitan import my_name
+
+def test_long_name():
+
+    answer = my_name(averylongname)
+
+    assert answer = "my name is averylongname"
+
+def test_short_name():
+
+    answer = my_name(name)
+
+    assert answer = "my name is name"
