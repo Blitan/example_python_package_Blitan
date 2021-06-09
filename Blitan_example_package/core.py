dcb8a5db6da81a7af33a990c42b3021550134921
Keir <Keirhearn@gmail.com>
2021-06-09 12:16:59 +0100

    added package

---
+++ Blitan_example_package/core.py
@@ -0,0 +1,4 @@
+
+def my_name(firstname)
+    print("my name is ", firstname)
+    return 'my name is' + firstname)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='example_python_package_Blitan',
+    author='Keir',
+    author_email='keirhearn@gmail.com',
+    license='MIT',
+    url='https://github.com/Blitan/example_python_package_Blitan',
+    description='XXX Skeleton python project example.',
+    packages=["example_python_package_Blitan"],
+    version="0.0.1"
+)
